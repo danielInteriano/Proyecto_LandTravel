5889f2adda26a8fcfd70f8d69b780216c4106614
Darkian98 <Darkian98@users.noreply.github.com>
2020-03-03 23:37:09 -0600

    Update getLugTuris-Ciudad.php

--- frontend/procesos/getLugTuris-Ciudad.php
+++ frontend/procesos/getLugTuris-Ciudad.php
@@ -5,7 +5,7 @@
 	$conexion=$obj->conexion();
 
 	$codigo=$_POST['id'];
-	$sql="select id_lugares_turisticos, descripcion from lugares_turisticos where id_ciudad=$codigo order by descripcion asc";
+	$sql="select idlugares_turisticos, descripcion from lugares_turisticos where idciudad=$codigo order by descripcion asc";
 	$result=mysqli_query($conexion,$sql);
 
 	while (	$ver=mysqli_fetch_row($result))
