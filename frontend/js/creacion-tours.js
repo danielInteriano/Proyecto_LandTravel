27b9a0b4b6794c7fc4f43bbeb886f61d5be9ab54
adeslive <adeslive@outlook.es>
2020-03-04 01:02:09 -0600

    a

---
+++ frontend/js/creacion-tours.js
@@ -0,0 +1,33 @@
+$(document).ready(function (){
+    console.log("Carga JS!");
+});
+
+$('#button-create').click(function(){
+
+	var parametros = 
+	{
+        idtipo_tour : $("#create-dias").val(),
+        nombre: $("#nombre").val(),
+        fecha_inicio: $("#fecha-inicio").val(),
+        cupo: $("#cupo").val(),
+	};
+	
+	console.log(parametros);
+
+	$.ajax({
+		type:"POST",
+		data: parametros,
+		datatype:'Json',
+		url:"../php/nuevotour.php",
+		success:function(resultado){
+			console.log(resultado);
+			let res = JSON.parse(resultado);
+		
+			if(res.creado === true){	
+                location.href = `creacion-rutas.php?id=${res.idtour}`;
+			}else{
+				alerty.error(res.mensaje);
+			}
+		}
+	});
+});
