fc9755a17ce219c8d5047023783dfea20a6926be
Daniel Stanly <60996823+danielInteriano@users.noreply.github.com>
2020-03-04 18:25:38 -0600

    Update eliminarTour.php

--- frontend/php/eliminarTour.php
+++ frontend/php/eliminarTour.php
@@ -1,5 +1,5 @@
 <?php
-$id = $_POST['tour'];
+$id = $_GET['id'];
 $ch = curl_init('localhost:8080/tours/' . $id);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
